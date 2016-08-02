d3f75ab50926fb3a83ff29e3d967d32186034acc
Paul Scarrone <paul.scarrone@gmail.com>
2016-08-02 12:31:07 -0400

    can have specific accounts with individual smtp settings

--- tests/test.rb
+++ tests/test.rb
@@ -16,15 +16,39 @@ class MyTest < MiniTest::Test
   end
 
   def test_mailer
-    post '/', {name: 'butt',
-               email: 'butt@tp.com',
-               textarea: 'hi smelly',
-               action: 'value'}
+    post '/', { public_token: 'default',
+                name: 'butt',
+                email: 'butt@tp.com',
+                textarea: 'hi smelly',
+                action: 'value' }
 
     assert last_response.ok?
     delivered_email = ActionMailer::Base.deliveries.last
     assert_includes delivered_email.to, 'paul@savvysoftworks.com'
     assert_includes delivered_email.body, 'hi smelly'
     assert_includes delivered_email.subject, 'butt@tp.com'
+  end
+
+  def test_mailer_redirects_to_error_without_configured_account
+    post '/', {name: 'butt',
+               email: 'butt@tp.com',
+               textarea: 'hi smelly',
+               action: 'value'}
+
+    assert last_response.redirect?
+    follow_redirect!
+    assert_includes last_response.body, 'Account Not Configured'
+  end
+
+  def test_mailer_redirects_to_error_without_valid_configured_account
+    post '/', { public_token: 'no-account',
+                name: 'butt',
+                email: 'butt@tp.com',
+                textarea: 'hi smelly',
+                action: 'value' }
+
+    assert last_response.redirect?
+    follow_redirect!
+    assert_includes last_response.body, 'Account Not Configured'
   end
 end
